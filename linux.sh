38c5138a2e376c35bc27b3fd8b8cf5b01be0691b
sergey837 <58519426+sergey837@users.noreply.github.com>
2024-04-24 18:49:35 +0300

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,130 +1,123 @@
-#!/bin/bash
-ARGS=$@
+#!/bin/sh
+DEFAULT_DOWNLOAD="http://dl.9hits.com/3.0.4/9hitsv3-linux64.tar.bz2"
 _9HITSUSER="_9hits"
+
+set -- --default-dl=$DEFAULT_DOWNLOAD $@
+ARGS=$@
 dist="unknown"
 
-function main() {
-	[ "$(id -u)" != "0" ] && \
-		abort "This script must be executed as root."
-		
-	check_dist
-	
-	echo "Installing dependencies..."
-	case "${dist}" in
-		debian|ubuntu)
-			install_apt
-			;;
-		centos|fedora|rocky)
-			install_yum
-			;;
-		*)
-			not_supported
-			;;
-	esac
-	install_fonts
-	
-	check_custom_dir
-	get_app
+main() {
+    [ "$(id -u)" != "0" ] && \
+        abort "Этот скрипт должен выполняться от имени суперпользователя."
+        
+    check_dist
+    
+    echo "Установка зависимостей..."
+    case "${dist}" in
+        debian|ubuntu)
+            install_apt
+            ;;
+        alpine)
+            install_apk
+            ;;
+        centos|fedora|rocky)
+            install_yum
+            ;;
+        *)
+            not_supported
+            ;;
+    esac
+    
+    check_custom_dir
+    get_app
 }
 
-function get_app() {
-	if id "$_9HITSUSER" &>/dev/null; then
-		echo '9HITS LOG: user $_9HITSUSER created'
-	else
-		echo '9HITS LOG: creating $_9HITSUSER user'
-		useradd -m $_9HITSUSER
-	fi
+get_app() {
+    if id "$_9HITSUSER" &>/dev/null; then
+        echo "9HITS LOG: пользователь создан: $_9HITSUSER"
+    else
+        echo "9HITS LOG: создание пользователя: $_9HITSUSER"
+        adduser -D $_9HITSUSER
+    fi
 
-	wget -O "/tmp/get-app.sh" https://9hitste.github.io/install/get-app.sh
-	chmod +x "/tmp/get-app.sh"
-	runuser -l $_9HITSUSER -c "/bin/bash /tmp/get-app.sh $(printf "%q" "$ARGS")"
-	rm -f "/tmp/get-app.sh"
+    wget -O "/tmp/get-app.sh" https://9hitste.github.io/install/3.0.4/get-app.sh && chmod +x "/tmp/get-app.sh"
+    su - $_9HITSUSER -c "/bin/sh /tmp/get-app.sh $(printf "%q" "$ARGS")"
+    rm -f "/tmp/get-app.sh"
 }
 
-
-function check_custom_dir() {
-	for i in $ARGS; do
-	  case $i in
-		--install-dir=*)
-			if [ ! -d "${i#*=}" ];then
-				mkdir "${i#*=}"
-			fi
-			setfacl -R -m u:$_9HITSUSER:rwx "${i#*=}"
-			shift # past argument=value
-		  ;;
-		--cache-dir=*)
-			if [ ! -d "${i#*=}" ];then
-				mkdir "${i#*=}"
-			fi
-			setfacl -R -m u:$_9HITSUSER:rwx "${i#*=}"
-			shift # past argument=value
-		  ;;
-		*)
-		  ;;
-	  esac
-	done
+check_custom_dir() {
+    for i in $ARGS; do
+      case $i in
+        --install-dir=*)
+            if [ ! -d "${i#*=}" ];then
+                mkdir -p "${i#*=}"
+            fi
+            chown $_9HITSUSER: "${i#*=}"
+            shift # past argument=value
+          ;;
+        --cache-dir=*)
+            if [ ! -d "${i#*=}" ];then
+                mkdir -p "${i#*=}"
+            fi
+            chown $_9HITSUSER: "${i#*=}"
+            shift # past argument=value
+          ;;
+        *)
+          ;;
+      esac
+    done
 }
 
-
-function install_fonts() {
-	if [ -d /usr/share/fonts/windows/ ]; then
-		echo "Skipping fonts installation"
-	else
-		echo "Installing fonts..."
-		rm -rf fonts.tar.bz2
-		wget http://dl.9hits.com/fonts.tar.bz2
-		tar -xvf fonts.tar.bz2 -C /
-		rm -rf fonts.tar.bz2
-		fc-cache -f -v
-	fi
+install_apt() {
+    DEBIAN_FRONTEND=noninteractive apt-get update
+    DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
+    DEBIAN_FRONTEND=noninteractive apt-get install -y unzip acl cron xvfb bzip2 libcanberra-gtk-module libxss1 htop sed tar libxtst6 libnss3 wget psmisc bc libgtk-3-0 libgbm-dev libatspi2.0-0 libatomic1
 }
 
-function install_apt() {
-	DEBIAN_FRONTEND=noninteractive apt-get update
-	DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
-	DEBIAN_FRONTEND=noninteractive apt-get install -y acl fontconfig cron xvfb bzip2 libcanberra-gtk-module libxss1 htop sed tar libxtst6 libnss3 wget psmisc bc libgtk-3-0 libgbm-dev libatspi2.0-0 libatomic1
+install_apk() {
+    apk update
+    apk upgrade
+    apk add --no-cache unzip acl dcron xvfb bzip2 libcanberra-gtk3 libxss htop sed tar libxtst libnss3 wget psmisc bc gtk+3.0 gcompat at-spi2-atk at-spi2-core
 }
 
-function install_yum() {
-	yum update -y
-	yum install -y acl fontconfig cronie libatomic alsa-lib-devel gtk3-devel libgbm libxkbcommon-x11 cups-libs.i686 cups-libs.x86_64 atk.x86_64 libnss3.so xorg-x11-server-Xvfb sed tar Xvfb wget bzip2 libXScrnSaver psmisc
+install_yum() {
+    yum update -y
+    yum install -y unzip acl cronie libatomic alsa-lib-devel gtk3-devel libgbm libxkbcommon-x11 cups-libs.i686 cups-libs.x86_64 atk.x86_64 libnss3.so xorg-x11-server-Xvfb sed tar Xvfb wget bzip2 libXScrnSaver psmisc
 }
 
-function check_dist() {
-	echo -n "Verifying compatibility with 9hits..."
-	if [  -f /etc/os-release  ]; then
-		dist=$(awk -F= '$1 == "ID" {gsub("\"", ""); print$2}' /etc/os-release)
-	elif [ -f /etc/redhat-release ]; then
-		dist=$(awk '{print tolower($1)}' /etc/redhat-release)
-	else
-		not_supported
-	fi
+check_dist() {
+    echo -n "Проверка совместимости с 9hits..."
+    if [  -f /etc/os-release  ]; then
+        dist=$(awk -F= '$1 == "ID" {gsub("\"", ""); print$2}' /etc/os-release)
+    elif [ -f /etc/redhat-release ]; then
+        dist=$(awk '{print tolower($1)}' /etc/redhat-release)
+    else
+        not_supported
+    fi
 
-	dist=$(echo "${dist}" | tr '[:upper:]' '[:lower:]')
+    dist=$(echo "${dist}" | tr '[:upper:]' '[:lower:]')
 
-	case "${dist}" in
-		debian|ubuntu|centos|fedora|rocky)
-			echo "OK"
-			;;
-		*)
-			not_supported
-			;;
-	esac
+    case "${dist}" in
+        debian|ubuntu|centos|fedora|rocky|alpine)
+            echo "OK"
+            ;;
+        *)
+            not_supported
+            ;;
+    esac
 }
 
-
-function not_supported() {
-	cat <<-EOF
-	The 9Hits App does not support the OS/Distribution on this machine.
-	EOF
-	exit 1
+not_supported() {
+    cat <<-EOF
+    Приложение 9Hits не поддерживает данную ОС/дистрибутив на этом компьютере.
+    EOF
+    exit 1
 }
 
-# abort with an error message
-function abort() {
-	read -r line func file <<< "$(caller 0)"
-	echo "ERROR in $file:$func:$line: $1" > /dev/stderr
-	exit 1
+abort() {
+    read -r line func file <<< "$(caller 0)"
+    echo "ОШИБКА в $file:$func:$line: $1" > /dev/stderr
+    exit 1
 }
 
 main
