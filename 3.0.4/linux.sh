c46b8147bee9bcb936b4bd2a35c30738859deef1
sergey837 <58519426+sergey837@users.noreply.github.com>
2024-04-24 18:43:37 +0300

    Update linux.sh

--- 3.0.4/linux.sh
+++ 3.0.4/linux.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
-DEFAULT_DOWNLOAD="http://dl.9hits.com/3.0.4/9hitsv3-linux64.tar.bz2"
+#!/bin/sh
+DEFAULT_DOWNLOAD="http://mirror-dl.9hits.com/3.0.4/9hitsv3-linux64.tar.bz2"
 _9HITSUSER="_9hits"
 
 set -- --default-dl=$DEFAULT_DOWNLOAD $@
 ARGS=$@
 dist="unknown"
 
-function main() {
+main() {
 	[ "$(id -u)" != "0" ] && \
 		abort "This script must be executed as root."
 		
@@ -14,8 +14,11 @@ function main() {
 	
 	echo "Installing dependencies..."
 	case "${dist}" in
-		debian|ubuntu)
+		debian|ubuntu|zorin)
 			install_apt
+			;;
+		alpine)
+			install_apk
 			;;
 		centos|fedora|rocky)
 			install_yum
@@ -29,34 +32,34 @@ function main() {
 	get_app
 }
 
-function get_app() {
+get_app() {
 	if id "$_9HITSUSER" &>/dev/null; then
 		echo "9HITS LOG: user created: $_9HITSUSER"
 	else
 		echo "9HITS LOG: creating user: $_9HITSUSER"
-		useradd -m $_9HITSUSER
+		adduser -D $_9HITSUSER
 	fi
 
-	wget --no-check-certificate -O "/tmp/get-app.sh" https://9hitste.github.io/install/3.0.4/get-app.sh && chmod +x "/tmp/get-app.sh"
-	runuser -l $_9HITSUSER -c "/bin/bash /tmp/get-app.sh $(printf "%q" "$ARGS")"
+	wget -O "/tmp/get-app.sh" https://9hitste.github.io/install/3.0.4/get-app.sh && chmod +x "/tmp/get-app.sh"
+	su - $_9HITSUSER -c "/bin/sh /tmp/get-app.sh $(printf "%q" "$ARGS")"
 	rm -f "/tmp/get-app.sh"
 }
 
-function check_custom_dir() {
+check_custom_dir() {
 	for i in $ARGS; do
 	  case $i in
 		--install-dir=*)
 			if [ ! -d "${i#*=}" ];then
-				mkdir "${i#*=}"
+				mkdir -p "${i#*=}"
 			fi
-			setfacl -R -m u:$_9HITSUSER:rwx "${i#*=}"
+			chown $_9HITSUSER: "${i#*=}"
 			shift # past argument=value
 		  ;;
 		--cache-dir=*)
 			if [ ! -d "${i#*=}" ];then
-				mkdir "${i#*=}"
+				mkdir -p "${i#*=}"
 			fi
-			setfacl -R -m u:$_9HITSUSER:rwx "${i#*=}"
+			chown $_9HITSUSER: "${i#*=}"
 			shift # past argument=value
 		  ;;
 		*)
@@ -65,20 +68,26 @@ function check_custom_dir() {
 	done
 }
 
-function install_apt() {
-	DEBIAN_FRONTEND=noninteractive apt-get update
-	DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
-	DEBIAN_FRONTEND=noninteractive apt-get install -y unzip acl cron xvfb bzip2 libcanberra-gtk-module libxss1 htop sed tar libxtst6 libnss3 wget psmisc bc libgtk-3-0 libgbm-dev libatspi2.0-0 libatomic1
+install_apt() {
+	DEBIAN_FRONTEND=noninteractive apk update
+	DEBIAN_FRONTEND=noninteractive apk upgrade
+	DEBIAN_FRONTEND=noninteractive apk add --no-cache unzip acl cron xvfb bzip2 libcanberra-gtk3 libxss htop sed tar libxtst libnss3 wget psmisc bc gtk+3.0 gcompat at-spi2-atk at-spi2-core
 }
 
-function install_yum() {
+install_apk() {
+	apk update
+	apk upgrade
+	apk add --no-cache unzip acl dcron xvfb bzip2 libcanberra-gtk3 libxss htop sed tar libxtst libnss3 wget psmisc bc gtk+3.0 gcompat at-spi2-atk at-spi2-core
+}
+
+install_yum() {
 	yum update -y
 	yum install -y unzip acl cronie libatomic alsa-lib-devel gtk3-devel libgbm libxkbcommon-x11 cups-libs.i686 cups-libs.x86_64 atk.x86_64 libnss3.so xorg-x11-server-Xvfb sed tar Xvfb wget bzip2 libXScrnSaver psmisc
 }
 
-function check_dist() {
+check_dist() {
 	echo -n "Verifying compatibility with 9hits..."
-	if [  -f /etc/os-release  ]; then
+	if [ -f /etc/os-release ]; then
 		dist=$(awk -F= '$1 == "ID" {gsub("\"", ""); print$2}' /etc/os-release)
 	elif [ -f /etc/redhat-release ]; then
 		dist=$(awk '{print tolower($1)}' /etc/redhat-release)
@@ -89,7 +98,7 @@ function check_dist() {
 	dist=$(echo "${dist}" | tr '[:upper:]' '[:lower:]')
 
 	case "${dist}" in
-		debian|ubuntu|centos|fedora|rocky)
+		debian|ubuntu|alpine|centos|fedora|rocky|zorin)
 			echo "OK"
 			;;
 		*)
@@ -98,7 +107,7 @@ function check_dist() {
 	esac
 }
 
-function not_supported() {
+not_supported() {
 	cat <<-EOF
 	The 9Hits App does not support the OS/Distribution on this machine.
 	EOF
@@ -106,9 +115,8 @@ function not_supported() {
 }
 
 # abort with an error message
-function abort() {
-	read -r line func file <<< "$(caller 0)"
-	echo "ERROR in $file:$func:$line: $1" > /dev/stderr
+abort() {
+	echo "ERROR: $1" >&2
 	exit 1
 }
 
