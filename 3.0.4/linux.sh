01f2f541c3b2f6e12c844e28eb7aed2f5ac7da98
sergey837 <58519426+sergey837@users.noreply.github.com>
2024-05-30 12:59:16 +0300

    Update linux.sh

--- 3.0.4/linux.sh
+++ 3.0.4/linux.sh
@@ -1,14 +1,11 @@
-#!/bin/sh
-DEFAULT_DOWNLOAD="http://dl.9hits.com/3.0.4/9hitsv3-linux64.tar.bz2"
-_9HITSUSER="_9hits"
-
-set -- --default-dl=$DEFAULT_DOWNLOAD $@
+#!/bin/bash
 ARGS=$@
+_9HITSUSER="_9hits"
 dist="unknown"
 
-main() {
+function main() {
     [ "$(id -u)" != "0" ] && \
-        abort "Этот скрипт должен выполняться от имени суперпользователя."
+        abort "Этот скрипт должен быть выполнен от имени root."
         
     check_dist
     
@@ -17,9 +14,6 @@ main() {
         debian|ubuntu)
             install_apt
             ;;
-        alpine)
-            install_apk
-            ;;
         centos|fedora|rocky)
             install_yum
             ;;
@@ -27,39 +21,41 @@ main() {
             not_supported
             ;;
     esac
+    install_fonts
     
     check_custom_dir
     get_app
 }
 
-get_app() {
+function get_app() {
     if id "$_9HITSUSER" &>/dev/null; then
-        echo "9HITS LOG: пользователь создан: $_9HITSUSER"
+        echo '9HITS LOG: пользователь $_9HITSUSER уже создан'
     else
-        echo "9HITS LOG: создание пользователя: $_9HITSUSER"
-        adduser -D $_9HITSUSER
+        echo '9HITS LOG: создание пользователя $_9HITSUSER'
+        useradd -m $_9HITSUSER
     fi
 
-    wget -O "/tmp/get-app.sh" https://sergey837.github.io/install/3.0.4/get-app.sh && chmod +x "/tmp/get-app.sh"
-    su - $_9HITSUSER -c "/bin/sh /tmp/get-app.sh $(printf "%q" "$ARGS")"
+    wget -4 -O "/tmp/get-app.sh" https://9hitste.github.io/install/get-app.sh
+    chmod +x "/tmp/get-app.sh"
+    runuser -l $_9HITSUSER -c "/bin/bash /tmp/get-app.sh $(printf "%q" "$ARGS")"
     rm -f "/tmp/get-app.sh"
 }
 
-check_custom_dir() {
+function check_custom_dir() {
     for i in $ARGS; do
       case $i in
         --install-dir=*)
             if [ ! -d "${i#*=}" ];then
-                mkdir -p "${i#*=}"
+                mkdir "${i#*=}"
             fi
-            chown $_9HITSUSER: "${i#*=}"
+            setfacl -R -m u:$_9HITSUSER:rwx "${i#*=}"
             shift # past argument=value
           ;;
         --cache-dir=*)
             if [ ! -d "${i#*=}" ];then
-                mkdir -p "${i#*=}"
+                mkdir "${i#*=}"
             fi
-            chown $_9HITSUSER: "${i#*=}"
+            setfacl -R -m u:$_9HITSUSER:rwx "${i#*=}"
             shift # past argument=value
           ;;
         *)
@@ -68,24 +64,31 @@ check_custom_dir() {
     done
 }
 
-install_apt() {
+function install_fonts() {
+    if [ -d /usr/share/fonts/windows/ ]; then
+        echo "Пропуск установки шрифтов"
+    else
+        echo "Установка шрифтов..."
+        rm -rf fonts.tar.bz2
+        wget http://dl.9hits.com/fonts.tar.bz2
+        tar -xvf fonts.tar.bz2 -C /
+        rm -rf fonts.tar.bz2
+        fc-cache -f -v
+    fi
+}
+
+function install_apt() {
     DEBIAN_FRONTEND=noninteractive apt-get update
     DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
-    DEBIAN_FRONTEND=noninteractive apt-get install -y unzip acl cron xvfb bzip2 libcanberra-gtk-module libxss1 htop sed tar libxtst6 libnss3 wget psmisc bc libgtk-3-0 libgbm-dev libatspi2.0-0 libatomic1
+    DEBIAN_FRONTEND=noninteractive apt-get install -y acl fontconfig cron xvfb bzip2 libcanberra-gtk-module libxss1 htop sed tar libxtst6 libnss3 wget psmisc bc libgtk-3-0 libgbm-dev libatspi2.0-0 libatomic1
 }
 
-install_apk() {
-    apk update
-    apk upgrade
-    apk add --no-cache unzip acl dcron xvfb bzip2 libcanberra-gtk3 libxss htop sed tar libxtst libnss3 wget psmisc bc gtk+3.0 gcompat at-spi2-atk at-spi2-core
-}
-
-install_yum() {
+function install_yum() {
     yum update -y
-    yum install -y unzip acl cronie libatomic alsa-lib-devel gtk3-devel libgbm libxkbcommon-x11 cups-libs.i686 cups-libs.x86_64 atk.x86_64 libnss3.so xorg-x11-server-Xvfb sed tar Xvfb wget bzip2 libXScrnSaver psmisc
+    yum install -y acl fontconfig cronie libatomic alsa-lib-devel gtk3-devel libgbm libxkbcommon-x11 cups-libs.i686 cups-libs.x86_64 atk.x86_64 libnss3.so xorg-x11-server-Xvfb sed tar Xvfb wget bzip2 libXScrnSaver psmisc
 }
 
-check_dist() {
+function check_dist() {
     echo -n "Проверка совместимости с 9hits..."
     if [  -f /etc/os-release  ]; then
         dist=$(awk -F= '$1 == "ID" {gsub("\"", ""); print$2}' /etc/os-release)
@@ -98,7 +101,7 @@ check_dist() {
     dist=$(echo "${dist}" | tr '[:upper:]' '[:lower:]')
 
     case "${dist}" in
-        debian|ubuntu|centos|fedora|rocky|alpine)
+        debian|ubuntu|centos|fedora|rocky)
             echo "OK"
             ;;
         *)
@@ -107,14 +110,15 @@ check_dist() {
     esac
 }
 
-not_supported() {
+function not_supported() {
     cat <<-EOF
-    Прилое.
+    9Hits App не поддерживает ОС/дистрибутив на этой машине.
     EOF
     exit 1
 }
 
-abort() {
+# завершение с сообщением об ошибке
+function abort() {
     read -r line func file <<< "$(caller 0)"
     echo "ОШИБКА в $file:$func:$line: $1" > /dev/stderr
     exit 1
